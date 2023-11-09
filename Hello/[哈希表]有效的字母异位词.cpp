c939b8fe385ebd59fe1ea2f9d271b84e76fdb541
Sagasu <62923489+lvyeme@users.noreply.github.com>
2023-11-09 17:59:48 +0800

    有效的字母异位词

---
+++ Hello/[哈希表]有效的字母异位词.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+
+	bool isAnagram(string s,string t) {
+	
+		int record[26] = { 0 };
+		for (int i = 0; i < s.size(); i++) {
+			// 并不需要记住字符a的ASCII，只要求出一个相对数值就可以了
+			record[s[i] - 'a']++;
+		}
+		for (int i = 0; i < t.size(); i++) {
+			record[t[i] - 'a']--;
+		}
+		for (int i = 0; i < 26; i++) {
+			if (record[i] != 0) {
+				// record数组如果有的元素不为零0，说明字符串s和t 一定是谁多了字符或者谁少了字符。
+				return false;
+			}
+		}
+		//record数组所有元素都为零，说明字符串s和t是字母异位词
+		return true;
+	}
+
+	/*bool isAnagram(string s, string t) {
+		int count[26] = { 0 };
+		for (char c : s) count[c - 'a']++;
+		for (char c : t) count[c - 'a']--;
+		for (int i = 0; i < 26; i++) if (count[i]) return false;
+		return true;
+	}*/
+};
+
+int main() {
+	Solution slou;
+	string s = "qwer";
+	string t = "qwer";
+
+	bool result = slou.isAnagram(s, t);
+	cout << result << endl;
+
+	return 0;
+}
+
