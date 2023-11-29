de778a933e52968230ce4325b429b683362f566d
Sagasu <62923489+lvyeme@users.noreply.github.com>
2023-11-29 10:06:37 -0600

    迭代

---
+++ Hello/前序.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+	
+public:
+	vector<int> inorderTraversal(TreeNode* root) {
+	
+		stack<TreeNode*> st;
+		vector<int> result;
+		TreeNode* cur = root;
+
+		if (root == NULL)return result;
+		st.push(root);
+		while (!st.empty())
+		{
+			TreeNode* node = st.top();
+			st.pop();
+			result.push_back(node->val);
+			if (node->right)st.push(node->right);
+			if (node->left)st.push(node->left);
+		}
+		return result;
+	}
+};
+
+int main() {
+	// 创建一个简单的二叉树
+	TreeNode* root = new TreeNode(1);
+	root->left = new TreeNode(2);
+	root->right = new TreeNode(3);
+	root->left->left = new TreeNode(4);
+	root->left->right = new TreeNode(5);
+
+	// 使用 Solution 类的 preorderTraversal 函数进行预订序遍历
+	Solution solution;
+	vector<int> traversalResult = solution.inorderTraversal(root);
+
+	// 输出预订序遍历的结果
+	cout << "Preorder Traversal Result:" << endl;
+	for (int val : traversalResult) {
+		cout << val << " ";
+	}
+	cout << endl;
+}
--- Hello/有效括号.cpp
+++ Hello/有效括号.cpp
@@ -18,10 +18,33 @@ public:
 			if (s[i] == '(') st.push(')');
 			else if (s[i] == '{') st.push('}');
 			else if (s[i] == '[') st.push(']');
-			else if (st.empty || st.top() != s[i])
+			else if (st.empty() || st.top() != s[i])
 				return false;
-
+			else st.pop();
 		}
+		return st.empty();
 	}
 
 };
+
+int main() {
+	Solution solution;
+
+	// ²âÊÔ×Ö·û´®
+	string input1 = "()";
+	string input2 = "()[]{}";
+	string input3 = "(]";
+	string input4 = "([)]";
+	string input5 = "{[]}";
+
+	// ²âÊÔ²¢Êä³ö½á¹û
+	cout << "Input: " << input1 << " -> Valid: " << solution.isValid(input1) << endl;
+	cout << "Input: " << input2 << " -> Valid: " << solution.isValid(input2) << endl;
+	cout << "Input: " << input3 << " -> Valid: " << solution.isValid(input3) << endl;
+	cout << "Input: " << input4 << " -> Valid: " << solution.isValid(input4) << endl;
+	cout << "Input: " << input5 << " -> Valid: " << solution.isValid(input5) << endl;
+
+	return 0;
+}
+
+
