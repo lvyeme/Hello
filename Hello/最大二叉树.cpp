1dee41ce62136b6b91831bc23f712cae78016bad
Sagasu <62923489+lvyeme@users.noreply.github.com>
2024-01-20 21:51:36 -0600

    constructMaximumBinaryTree

---
+++ Hello/最大二叉树.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+
+private:
+	//在左闭右开区间[left,right),构造二叉树
+	TreeNode* traversal(vector<int>& nums, int left, int right) { 
+	
+		if (left >= right)return nullptr;
+
+		//分割点下标:macValueIndex
+		int maxValueIndex = left;
+		for (int i = left + 1; i < right; i++) {
+		
+			if (nums[i] > nums[maxValueIndex])maxValueIndex = i;
+		}
+		TreeNode* root = new TreeNode(nums[maxValueIndex]);
+
+		//左闭右开[left,maxValueIndex)
+		root->left = traversal(nums, left, maxValueIndex);
+
+		//左闭右开[maxValueIndex + 1 , right)
+		root->right = traversal(nums, maxValueIndex + 1, right);
+
+		return root;
+	} 
+
+public:
+	TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+	
+		return traversal(nums, 0, nums.size());
+	}
+
+};
+
+
+
