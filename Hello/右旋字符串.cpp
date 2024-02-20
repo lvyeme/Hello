da96876f3988c291f8824e8de8a8ed9f69115c1d
Sagasu <62923489+lvyeme@users.noreply.github.com>
2024-02-19 21:07:18 -0600

    右旋字符串

--- Hello/右旋字符串.cpp
+++ Hello/右旋字符串.cpp
@@ -2,3 +2,15 @@
 #include<algorithm>
 using namespace std;
 
+int main(){
+	int n;
+	string s;
+	cin >> n;
+	cin >> s;
+	int len = s.size();//获取长度
+
+	reverse(s.begin(), s.end());//整体翻转
+	reverse(s.begin(), s.begin()+n);//先反转前一段，长度n
+	reverse(s.begin() + n, s.end());//再翻转后一段
+	cout << s << endl;
+}
